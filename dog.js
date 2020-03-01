461ee9f7ed90c7acc58aaf121023516fbdcf6e7c
leducloi <dekaito193@gmail.com>
2020-03-01 22:05:09 +0700

    add sayhi method

--- dog.js
+++ dog.js
@@ -1,7 +1,14 @@
-function Dog {
-	
+var chalk = require('chalk');
+function Dog(name) {
+	this.name = name;
 }
 
-function hello {
+function hello() {
 	console.log('hello');
 }
+
+Dog.prototype.sayhi = function() {
+	console.log('say hi, ' + chalk.red(this.name));
+};
+
+module.exports = Dog;
---
+++ index.js
@@ -0,0 +1,4 @@
+var dog = require('./Dog');
+
+var abc = new Dog('Tom');
+abc.sayhi();
